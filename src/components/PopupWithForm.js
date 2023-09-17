e48ee9309f9c267376463cf9e9b5faa60d3a3abb
Matthew Peldunas <peldunasma@gmail.com>
2023-09-16 21:44:38 -0400

    add PopupWithForm

---
+++ src/components/PopupWithForm.js
@@ -0,0 +1,46 @@
+import Popup from "./Popup.js"; 
+
+export default class PopupWithForm extends Popup {
+    constructor(popupSelector, handleFormSubmit) {
+        //instantiantes Popup class (parent class)
+        super({ popupSelector });
+        this._popupForm = this._popupElement.querySelector('.modal__form');
+        this._popupCloseForm = this._popupElement.querySelector('.modal__close');
+        this._handleFormSubmit = handleFormSubmit;
+        this._inputList = this._popupElement.querySelectorAll('.modal__input');
+
+    }
+
+    _getInputValues(){
+        //create an empty array
+        const inputValues = {};
+        //create a forEach loop to take in the input values  
+        this._inputList.forEach((input) => (inputValues[input.id] = input.value));
+        return inputValues;
+
+    }
+
+    setEventListeners() {
+       this._popupForm.addEventListener('submit', () => {
+        this.handleFormSubmit(this._getInputValues());
+       }); 
+       this._popupCloseForm.addEventListener('click', () => {
+        this.close();
+       })
+        super.setEventListeners();      
+    }
+
+    close() {
+        this._popupForm.reset();
+        super.close();
+    }
+
+}
+
+// index.js 
+
+// const newFormPopup = new PopupWithForm('#profile-add-modal', () => {});
+// newFormPopup.open()
+
+// newFormPopup.close();
+
